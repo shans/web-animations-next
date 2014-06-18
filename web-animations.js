16bfc43d120dd175dbd0cf857c47876229101791
rjwright <rjwright@chromium.org>
2014-06-18 12:12:01 +1000

    Pause and play children of a group
    Trying to sort out setting start time
    
    fixes after rebase. Time for another one!
    
    Setting startTime on group players is working
    
    Absolute hack job but setting current time on paused and playing groups seems to work.
    Need to fix finished() for group players, among other things.
    Need to compare against master to make sure I haven't removed anything important.
    
    Cleaned up code. Setting startTime, currentTime, on groups, pausing and playing groups
    Seems to be working.
    
    Share player prototype with group players. Combine play() code for both types of groups.
    
    Fixed some breaking tests
    
    Small cleanup
    
    Reverse() for groups sort of works. Going to change _startOffset
    to be absolute, not relative to the parent.
    
    Alternate offsets seem to be working
    
    Comments
    
    Comments
    
    Cleanup.
    
    Changed play
    
    Changed the way group players are made. Reversing works
    repeatedly on groups now, but probably won't work if
    the group has a start delay (or its start time isn't
    0 for some other reason).
    
    Change the way player.play calculates startTmie
    
    offsets for reverse.

--- web-animations.js
+++ web-animations.js
@@ -35,6 +35,8 @@ if (this.TESTING === undefined) {
     'src/timeline.js',
     // --- maxifill below here --- //
     'src/animation-constructor.js',
+    'src/group-constructors.js',
+    'src/group-player.js',
   ];
 
   if (typeof module != 'undefined') {
